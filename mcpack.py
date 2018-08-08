ff46b6b26d87ef5c1593329ed59cc6b2446064cb
Valentin Berlier <berlier.v@gmail.com>
2018-08-08 02:06:12 +0200

    Version 0.3.0

--- mcpack.py
+++ mcpack.py
@@ -26,7 +26,7 @@ __all__ = ['DataPack', 'Namespace', 'Advancement', 'Function', 'LootTable',
            'Recipe', 'Structure', 'BlockTag', 'ItemTag', 'FluidTag',
            'FunctionTag']
 
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 
 import json
 import pathlib
